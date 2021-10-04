05f01a38cb32ab689ca5c0d96b3896fe002bfd9b
Tamara Nawrocki <tamara@sztanski.com>
2021-10-04 18:20:13 -0500

    n number commit

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import Spinner from "./components/Spinner";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [url, setUrl] = useState("http://localhost:8000/blogs");
+  const { data, isPending, error } = useFetch(url);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "mario")}
-        title="Mario's blogs"
-      />
+      {error && <div>{error}</div>}
+      {isPending && <Spinner />}
+      {data && <BlogList blogs={data} title="All Blogs" />}
     </div>
   );
 };
